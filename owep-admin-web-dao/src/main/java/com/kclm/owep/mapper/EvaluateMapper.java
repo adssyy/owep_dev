a444b637065376834107764aa89de62ddea6affb
shentr <740364375@qq.com>
2020-07-06 18:23:46 +0800

    修改学员作业完成的实体类

--- owep-admin-web-dao/src/main/java/com/kclm/owep/mapper/EvaluateMapper.java
+++ owep-admin-web-dao/src/main/java/com/kclm/owep/mapper/EvaluateMapper.java
@@ -1,7 +1,15 @@
+/*********************
+ * 2018-2021 版权所有 CopyRight（c） 快程乐码信息科技有限公司所有，未经授权，不得复制、转发
+ */
 package com.kclm.owep.mapper;
 
 import com.kclm.owep.entity.Evaluate;
-
+/*********************
+ * @Auther shentr
+ * @Version V1.0
+ * @Create 2020/7/6 18:20
+ * @Description 学员评价持久层接口
+ */
 public interface EvaluateMapper {
 
     /**
--- owep-admin-web-dao/src/main/java/com/kclm/owep/mapper/IllegalMapper.java
+++ owep-admin-web-dao/src/main/java/com/kclm/owep/mapper/IllegalMapper.java
@@ -1,3 +1,6 @@
+/*********************
+ * 2018-2021 版权所有 CopyRight（c） 快程乐码信息科技有限公司所有，未经授权，不得复制、转发
+ */
 package com.kclm.owep.mapper;
 
 import com.kclm.owep.entity.Illegal;
@@ -5,7 +8,12 @@ import com.kclm.owep.entity.Illegal;
 import java.time.LocalDate;
 import java.util.List;
 import org.apache.ibatis.annotations.Param;
-
+/*********************
+ * @Auther shentr
+ * @Version V1.0
+ * @Create 2020/7/6 18:20
+ * @Description 违规处理持久层接口
+ */
 public interface IllegalMapper {
     /**
     *根据班级id查询违纪处理的数据
