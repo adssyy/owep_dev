d288b8889463e9f052056aace1f8b5dfaae0f558
QI_Kran <1102354394@qq.com>
2020-07-06 12:10:54 +0800

    实体_权限管理_#I1LUI6 -----v0.1.1 用户组和角色Mapper

---
+++ owep-admin-web-dao/src/test/java/com/kclm/owep/mapper/privilege/GroupMapperTest.java
@@ -0,0 +1,64 @@
+package com.kclm.owep.mapper.privilege;
+
+import com.kclm.owep.mapper.ActionMapperTest;
+import com.kclm.owep.mapper.GroupMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @program: owep
+ * @package: com.kclm.owep.mapper.privilege
+ * @description:
+ * @author: qi_kran
+ * @create: 2020-07-05 20:16
+ **/
+class GroupMapperTest extends ActionMapperTest {
+@Autowired
+private GroupMapper groupMapper;
+    @Test
+    void count() {
+    }
+
+    @Test
+    void findByAll() {
+    }
+
+    @Test
+    void findByGroupName() {
+    }
+
+    @Test
+    void findByGroupId() {
+    }
+
+    @Test
+    void saveGroup() {
+        
+    }
+
+    @Test
+    void updateGroup() {
+    }
+
+    @Test
+    void deleteByIds() {
+    }
+
+    @Test
+    void saveRoleForGroup() {
+    }
+
+    @Test
+    void deleteByGroupId() {
+    }
+
+    @Test
+    void findRolesByGroupId() {
+    }
+
+    @Test
+    void countRolesByGroupId() {
+    }
+}
