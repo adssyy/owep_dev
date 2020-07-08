0a1bcead4b3fa7870dacacf551199727f2fc6e4b
yuyanCoder <2971543391@qq.com>
2020-07-08 22:25:41 +0800

    完善dao接口

---
+++ owep-admin-entity/src/main/java/com/kclm/owep/entity/Question.java
@@ -0,0 +1,183 @@
+package com.kclm.owep.entity;
+
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+/*********************
+ * @Author: tanj
+ * @Version : v1.0
+ * @Create: 2020年7月07日 上午10:10:40
+ * @Description 视频问答表
+ */
+public class Question implements Serializable {
+    /***
+     * id
+     */
+   private Integer id;
+    /***
+     * 问答时间
+     */
+   private LocalDateTime createTime=LocalDateTime.now();
+    /***
+     * 问答内容
+     */
+    private String questionContent;
+    /***
+     * 问答类型 1. 评论 0 .问题
+     */
+    private Integer questionType;
+    /***
+     * 提问者
+     */
+    private String questionAuthor;
+    /***
+     * 点赞数
+     */
+    private Integer favourCount;
+    /***
+     * 最后一次访问时间
+     */
+    private LocalDateTime lastAccessTime;
+    /***
+     * 版本号
+     */
+    private Integer version=1;
+    /***
+     * 视频
+     */
+    private Video video;
+    /**
+     * 学员
+     */
+    private Student student;
+    /**
+     * 班级
+     */
+    private Clazz clazz;
+
+    public Question() {
+    }
+
+    public Question(String questionContent, String questionAuthor) {
+        this.questionContent = questionContent;
+        this.questionAuthor = questionAuthor;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getCreateTime() {
+        return createTime;
+    }
+
+    public String getQuestionContent() {
+        return questionContent;
+    }
+
+    public void setQuestionContent(String questionContent) {
+        this.questionContent = questionContent;
+    }
+
+    public Integer getQuestionType() {
+        return questionType;
+    }
+
+    public void setQuestionType(Integer questionType) {
+        this.questionType = questionType;
+    }
+
+    public String getQuestionAuthor() {
+        return questionAuthor;
+    }
+
+    public void setQuestionAuthor(String questionAuthor) {
+        this.questionAuthor = questionAuthor;
+    }
+
+    public Integer getFavourCount() {
+        return favourCount;
+    }
+
+    public void setFavourCount(Integer favourCount) {
+        this.favourCount = favourCount;
+    }
+
+    public LocalDateTime getLastAccessTime() {
+        return lastAccessTime;
+    }
+
+    public void setLastAccessTime(LocalDateTime lastAccessTime) {
+        this.lastAccessTime = lastAccessTime;
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+
+    public Video getVideo() {
+        return video;
+    }
+
+    public void setVideo(Video video) {
+        this.video = video;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Clazz getClazz() {
+        return clazz;
+    }
+
+    public void setClazz(Clazz clazz) {
+        this.clazz = clazz;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Question that = (Question) o;
+        return Objects.equals(id, that.id) &&
+                Objects.equals(createTime, that.createTime) &&
+                Objects.equals(questionContent, that.questionContent) &&
+                Objects.equals(questionType, that.questionType) &&
+                Objects.equals(questionAuthor, that.questionAuthor);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, createTime, questionContent, questionType, questionAuthor);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("VideoQuestion{");
+        sb.append("id=").append(id);
+        sb.append(", createTime=").append(createTime);
+        sb.append(", questionContent='").append(questionContent).append('\'');
+        sb.append(", questionType=").append(questionType);
+        sb.append(", questionAuthor='").append(questionAuthor).append('\'');
+        sb.append(", favourCount=").append(favourCount);
+        sb.append(", lastAccessTime=").append(lastAccessTime);
+        sb.append(", version=").append(version);
+        sb.append('}');
+        return sb.toString();
+    }
+}
--- owep-admin-web-dao/src/main/java/com/kclm/owep/mapper/ClazzMapper.java
+++ owep-admin-web-dao/src/main/java/com/kclm/owep/mapper/ClazzMapper.java
@@ -21,25 +21,24 @@ import java.util.List;
  */
 @Mapper
 public interface ClazzMapper extends BaseMapper<Clazz> {
-
     /***
-     * 保存一条记录
+     * 增
      * @param entity
      * @return
      */
     @Override
     int save(Clazz entity);
 
-    /***
-     * 更新一条记录
+    /**
+     * 更新
      * @param entity
      * @return
      */
     @Override
     int update(Clazz entity);
 
-    /***
-     * 根据id删除一条记录
+    /**
+     * 根据id删除
      * @param id
      * @return
      */
@@ -47,7 +46,7 @@ public interface ClazzMapper extends BaseMapper<Clazz> {
     int deleteById(@Param("id") Serializable id);
 
     /***
-     * 删除多条记录
+     * 删除多个
      * @param idList
      * @return
      */
@@ -55,17 +54,60 @@ public interface ClazzMapper extends BaseMapper<Clazz> {
     int deleteSelect(@Param("idList") List<Serializable> idList);
 
     /***
-     * 更加id来查询一条记录
+     *根据id查询
      * @param id
      * @return
      */
     @Override
     Clazz selectById(@Param("id") Serializable id);
 
-    /***
-     * 查询所有的记录
+    /**
+     * 查询所有
      * @return
      */
     @Override
     List<Clazz> selectAll();
+
+    /**
+     * 多表查询 根据clazzid来查询clazz和对应的resource
+     * @param cid
+     * @return
+     */
+    Clazz selectClazzAndRes(@Param("cid") Serializable cid);
+
+    /***
+     * 多表查询 查询所有的clazz并对应的planManage
+     * @return
+     */
+    Clazz selectClazzAndPlan(@Param("cid") Serializable cid);
+
+    /***
+     * 向t_class_resource 中间表中插入数据
+     * @param cid
+     * @param rid
+     * @return
+     */
+    int saveClazzResource(@Param("cid") Serializable cid,@Param("rid") Serializable rid);
+
+    /***
+     * 删除班级-资源中间表
+     * @param cid
+     * @return
+     */
+    int deleteClazzResource(@Param("cid") Serializable cid,@Param("rid") Serializable rid);
+    /***
+     * 向t_class_planmanage 中间表中插入数据
+     * @param cid
+     * @param pid
+     * @return
+     */
+    int saveClazzPlanmanage(@Param("cid") Serializable cid,@Param("pid") Serializable pid);
+
+    /***
+     * 根据班级id来删除班级方案中间表
+     * @param cid
+     * @return
+     */
+    int deleteClazzPlanmanage(@Param("cid") Serializable cid);
+
 }
